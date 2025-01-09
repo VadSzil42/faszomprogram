423765028045e01a90f237c433a1ca509a771823
Solid Wild <kissgabor0110110@gmail.com>
2025-01-09 08:19:02 +0100

    korte

--- shit.py
+++ shit.py
@@ -1,13 +1,13 @@
-def isSzokoev(ev:int)
+def isSzokoev(ev:int):
     if ev % 4 == 0:
         return True
     else:
         return False
     
-ev_i = input("Év: ")
+ev_i = input("ev: ")
 match isSzokoev(ev_i):
     case True:
-        print("{0} szökőév.".format(ev_i))
+        print("{0} szokoev.".format(ev_i))
 
     case False:
-        print("{0} nem szökőév.".format(ev_i))
+        print("{0} nem szokoev.".format(ev_i))
