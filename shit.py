f2944c32e0154a36e74904c21084c56abb710993
Solid Wild <kissgabor0110110@gmail.com>
2025-01-09 08:17:31 +0100

    alma

--- shit.py
+++ shit.py
@@ -1,2 +1,13 @@
-szam = sum([1,2,3])
-print(szam)
+def isSzokoev(ev:int)
+    if ev % 4 == 0:
+        return True
+    else:
+        return False
+    
+ev_i = input("Év: ")
+match isSzokoev(ev_i):
+    case True:
+        print("{0} szökőév.".format(ev_i))
+
+    case False:
+        print("{0} nem szökőév.".format(ev_i))
