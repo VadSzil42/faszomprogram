f6459baa44aa574e06e5caa7fef06e1593c4d650
Solid Wild <kissgabor0110110@gmail.com>
2025-01-16 08:08:58 +0100

    szilva

--- shit.py
+++ shit.py
@@ -1,15 +1,3 @@
-def isSzokoev(ev:int):
-    if ev % 4 != 0:
-        return False
-    elif ev % 400 == 0:
-        return True
-    elif ev % 100 == 0:
-        return False
-    else:
-        return True
-    
-ev_i = int(input("ev: "))
-if isSzokoev(ev_i):
-    print(ev_i, "szokoev.")
-else:
-    print(ev_i, "nem szokoev.")
+def lnko(a:int,b:int):
+    while a != b:
+        print("Hello World")
