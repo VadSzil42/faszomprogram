fcbb39ca6e37322b37a16b6401f62e49999dbe22
Solid Wild <kissgabor0110110@gmail.com>
2025-01-30 08:09:46 +0100

    szilva

--- shit3.py
+++ shit3.py
@@ -1,3 +1,14 @@
-a = [5, 3, 7, 2, 1, 0]
-b = a.sort()
-print(b)
+alma = [5, 3, 7, 2, 1, 0]
+alma.sort()
+print(alma)
+
+
+# VAGY
+
+
+korte = [5, 3, 7, 2, 1, 0]
+korte_sort = sorted(korte)
+print(korte)
+
+
+# DONE
